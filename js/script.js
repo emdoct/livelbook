d0c2c7a2f35ced5b080767471018edc53ecdb341
emdoct <tigeran4989@yandex.ru>
2020-10-17 13:45:53 +1000

    scroll animation add, mobile adaptation add

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-let logoHide = document.querySelector('.logo');
 
-logoHide.onclick = function() {
+let btn = document.querySelector('.btn');
+setTimeout(slowShow, 300);
+
+function slowShow() {
 	let textHide = document.querySelector('.first-screen__text');
 	let bgHide = document.querySelector('.first-screen__bg');
-	let btn = document.querySelector('.btn');
+	let logoHide = document.querySelector('.logo');
 	textHide.classList.toggle('hidden');
 	bgHide.classList.toggle('hidden');
 	logoHide.classList.toggle('hidden');
+	btn.classList.toggle('hidden');
+	
+}
+
+
+btn.onclick = function() {
+	event.preventDefault();
+	let scroll = document.querySelector('.second-screen__anchor');
+	scroll.scrollIntoView({behavior: 'smooth'});
 }
